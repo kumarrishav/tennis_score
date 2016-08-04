50c10e72f2da0c64cedd34463db27f473f75e1eb
M <gajm@paypalcorp.com>
2016-08-04 17:48:23 +0530

    Added check

--- TennisMain.java
+++ TennisMain.java
@@ -8,20 +8,57 @@ class Player
     int set;
 }
 class ScoreBoard{
+	final int MINSCORE=6;
+	final int MINDIFF=2
     Player A;
     Player B;
 	static void setCheck(){
-	    if(A.game>=6 && A.game>=(B.game+2)){
+	    if(A.game>=MINSCORE && A.game>=(B.game+MINDIFF)){
 	        A.set++;
 	        A.game=0;
 	        B.game=0;
 	    }
-		if(B.game>=6 && B.game>=(A.game+2)){
+		if(B.game>=MINSCORE && B.game>=(A.game+MINDIFF)){
 	        B.set++;
 	        B.game=0;
 	        A.game=0;
 	    }
 	}
+	public static void Score(String str){
+             GameCheck(String str);
+             
+       }
+       public static void gameCheck(String str){
+             for(int i=0;i<str.length();i++){
+				char ch=s.charAt(i);
+                    if(ch=='a')
+                           A.point++;
+                    else
+                           B.point++;
+                    if(A.point >= 4 || B.point >= 4){
+                           
+                           if(Math.abs(A.point-B.point)>=2){
+                                 if(A.point>B.point){
+                                        A.game++;
+										setCheck();
+									}
+                                 else{
+                                        B.game++;
+										setcheck();
+									}
+                                 A.point=0;
+                                 B.point=0;
+                           }
+                           else{
+                                 if(A.point==B.point){
+                                        A.point=3;
+                                        B.point=3;
+                                 }
+                           }
+                    }
+             }
+       }
+
 }
 class TennisMain{
     public static void main(String args[])throws IOException{
