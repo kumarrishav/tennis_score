75e3c90ea416e26a5db7250ceed966edf30cbd27
amrpandey <amrpandey@paypal.com>
2016-08-04 18:24:07 +0530

    handled lowercase a input

--- TennisMain.java
+++ TennisMain.java
@@ -35,7 +35,7 @@ class ScoreBoard{
        public static void gameCheck(String str){
              for(int i=0;i<str.length();i++){
 				char ch=str.charAt(i);
-                    if(ch=='a')
+                    if(ch=='A')
                            A.point++;
                     else
                            B.point++;
